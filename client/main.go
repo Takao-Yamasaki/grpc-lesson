0f8724c206e75fb494978d749d5c7751ca0de4fc
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-07-23 18:47:11 +0900

    add:client-stream-client

--- client/main.go
+++ client/main.go
@@ -5,6 +5,8 @@ import (
 	"grpc-lesson/pb"
 	"io"
 	"log"
+	"os"
+	"time"
 
 	"fmt"
 
@@ -19,11 +21,12 @@ func main() {
 	defer conn.Close()
 
 	client := pb.NewFileServiceClient(conn)
-	callListFiles(client)
-	callDownload(client)
+	// callListFiles(client)
+	// callDownload(client)
+	CallUpload(client)
 }
 
-// Unary RPC
+// Unary RPC(Client側)
 func callListFiles(client pb.FileServiceClient) {
 	res, err := client.ListFiles(context.Background(), &pb.ListFilesRequest{})
 	if err != nil {
@@ -33,7 +36,7 @@ func callListFiles(client pb.FileServiceClient) {
 	fmt.Println(res.GetFilenames())
 }
 
-// サーバーストリーミング
+// サーバーストリーミングRPC(Client側)
 func callDownload(client pb.FileServiceClient) {
 	req := &pb.DownloadRequest{Filename: "name.txt"}
 	stream, err := client.DownLoad(context.Background(), req)
@@ -53,4 +56,48 @@ func callDownload(client pb.FileServiceClient) {
 		log.Printf("Response from Download(bytes): %v", res.GetData())
 		log.Printf("Response from Download(string): %v", string(res.GetData()))
 	}
+}
+
+// クライアントストリーミングRPC（Client側）
+func CallUpload(client pb.FileServiceClient) {
+	filename := "sports.txt"
+	path := "/go/src/workspace/storage/" + filename
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	stream, err := client.Upload(context.Background())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	buf := make([]byte, 5)
+
+	for {
+		n, err := file.Read(buf)
+		if n == 0 || err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		req := &pb.UploadRequest{Data: buf[:n]}
+		sendErr := stream.Send(req)
+		if sendErr != nil {
+			log.Fatalln(sendErr)
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("received data size: %v", res.GetSize())
 }
